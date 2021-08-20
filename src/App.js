d85f5cb26a4f77da60e822f7795e2035569f2533
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-20 16:11:35 +0800

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from './firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+import {useAuthState} from 'react-firebase-hooks/auth';
+
+
 function App() {
   return (
     <div className="App">
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA5qFnNDO6j8BKaXYn18pOncsTnPjQNeQ4",
+    authDomain: "memory-card-game-edca8.firebaseapp.com",
+    projectId: "memory-card-game-edca8",
+    storageBucket: "memory-card-game-edca8.appspot.com",
+    messagingSenderId: "1067448308500",
+    appId: "1:1067448308500:web:2f19a58e7a937041f4e801",
+    measurementId: "G-ZK041B1K3Z"
+});
+
+export default firebase;
