4d16d74119cb650ccdb9a1efef431265a338136f
ZumTheZazaKing <zahidi85543@gmail.com>
2021-08-21 17:08:33 +0800

    Added functions to the cards. Noice

--- src/Ingame.js
+++ src/Ingame.js
@@ -5,28 +5,36 @@ import javascript from './images/javascript.png';
 import nodejs from './images/nodejs.png';
 import css from './images/css.png';
 
+import { useState,useEffect } from 'react';
+
 
 export default function Ingame(props){
 
-    const images = [Angular,Vue,logo,Vue,logo,Angular,javascript,nodejs,css,javascript,nodejs,css];
+    const [images, setImages] = useState([Angular,Vue,logo,Vue,logo,Angular,javascript,nodejs,css,javascript,nodejs,css]);
 
-    let currentIndex = images.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = images[currentIndex];
-      images[currentIndex] = images[randomIndex];
-      images[randomIndex] = temporaryValue;
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+      
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+        return array;
     }
 
-    const handleClick = e =>{
+    useEffect(() => {
+        setImages(shuffle(images));
+    },[])
 
+    const handleClick = e =>{
 
         if(!e.target.querySelector("img"))return;
 
@@ -39,6 +47,8 @@ export default function Ingame(props){
 
         } else {
 
+            props.setSearchingPhase(false);
+            props.setPreviousCard(e.target);
 
             if(e.target.querySelector("img").src === props.previousCard.querySelector("img").src){
                 console.log("It matches!");
@@ -56,6 +66,10 @@ export default function Ingame(props){
                     e.target.style.transform = "rotateY(180deg)";
                     props.previousCard.style.transform = "rotateY(180deg)";
                 },1000)
+                setTimeout(() => {
+                    e.target.querySelector("img").className = "hide";
+                    props.previousCard.querySelector("img").className = "hide";
+                },1300)
 
             }
         }
